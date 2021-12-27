8edd32ef54783a7932bab77a630593de2d7a75a5
Julian Heinovski <heinovski@ccs-labs.org>
2021-12-27 19:10:35 +0100

    Fix import for higher linux kernel version

--- ch34x.c
+++ ch34x.c
@@ -18,7 +18,7 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/init.h>
-#include <linux/signal.h>
+#include <linux/sched/signal.h>
 #include <linux/slab.h>
 #include <linux/tty.h>
 #include <linux/tty_driver.h>
